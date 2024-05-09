8e301aed78fae1c879485fbff644964dae480397
RocioHD <nidiarociohd28@gmail.com>
2024-05-09 18:57:16 -0500

    creacion objetos creacion objetos

--- sistema/Libreria/urls.py
+++ sistema/Libreria/urls.py
@@ -2,13 +2,13 @@
 
 from django.urls import path
 from . import views
-
-
+from django.conf import settings
+from django.contrib.staticfiles.urls import static  
 
 urlpatterns = [
     path('',views.inicio,name='inicio'),
     path('nosotros',views.nosotros, name='nosotros'),
     path('libros',views.libros, name='libros'),
-    path('crear',views.crearLibro, name='crear'),
-    path('editar',views.editarLibro, name='editar'),
-]
+    path('libros/crear',views.crearLibro, name='crear'),
+    path('libros/editar',views.editarLibro, name='editar'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
